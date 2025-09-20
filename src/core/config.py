1046142d1ebcf9f238d5d7edbf7cf22518d8e9e5
whoisxx <whoisxx1@gmail.com>
2025-09-20 16:20:50 -0700

    adding auth check

--- src/core/config.py
+++ src/core/config.py
@@ -3,7 +3,10 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-#class Settings:
-#    DATABASE_URL: str = os.getenv("DATABASE_URL")
+class Settings:
+    # DATABASE_URL: str = os.getenv("DATABASE_URL")
+    FIREBASE_PROJECT_ID: str = os.getenv("FIREBASE_PROJECT_ID")
+    FIREBASE_PRIVATE_KEY: str = os.getenv("FIREBASE_PRIVATE_KEY", "").replace('\\n', '\n')
+    FIREBASE_CLIENT_EMAIL: str = os.getenv("FIREBASE_CLIENT_EMAIL")
 
 #settings = Settings()
